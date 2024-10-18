2b79f70aaf654aeb5055188aeaadd9888d1263f5
Stephane Masyn <stefimaz@gmail.com>
2024-10-18 11:33:27 -0400

    pseudo game structure

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 //creating the game
+//Define board structure:
+// { type: "empty" | "small" | "large", id: number, hit: boolean }
+//greeting
+//Asking what size board to play: 4, 5, or 6
+//printing the board
+//placing ship on the board 
+//handling user guesses
+//Game flow
+//end game
